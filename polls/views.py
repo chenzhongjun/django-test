30c33909b18038ba392b1c2a341b7c4c10d0b7dc
server1 <eclipse@chen_zhongjun@live.com>
2017-06-28 22:48:59 +0800

    "new pull"

--- polls/views.py
+++ polls/views.py
@@ -6,9 +6,7 @@ from .models import Question, Choice
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = { 
-    'latest_question_list': latest_question_list, 
-}
+    context = {'latest_question_list': latest_question_list}
     return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
